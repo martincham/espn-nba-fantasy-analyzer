74148b09b7cd0706b8ab7362c57a9c0ca1633e17
martincham <martinchamberlin12@gmail.com>
2024-11-02 11:03:32 -0400

    add matchups

---
+++ library/matchups.py
@@ -0,0 +1,10 @@
+import library.schedule as schedule
+import library.config as config
+import library.rating as rating
+
+from datetime import date
+
+# 1. League matchup schedule
+# 2. For each matchup, get games each team will play on each day, and value of players playing
+# 3. Create projected value for each team in the matchup
+# 4. Maybe also calculate each of the 9 categories for each team 
